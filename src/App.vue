4ac7147ab744700212ed0e3222ced9e30af461aa
バントウフウマ <coolishpoo3@gmail.com>
2021-03-18 20:58:42 +0900

    add link button home and users

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="link-nav">
+      <router-link to="/" class="link">Home</router-link>
+      <router-link to="/users" class="link">Users</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.link-nav {
+  width: 700px;
+  margin: auto;
+  padding-top: 5rem;
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.link {
+  margin-right: 2rem;
 }
 </style>
