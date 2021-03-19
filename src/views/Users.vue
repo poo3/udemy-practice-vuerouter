600ae99ad7405dc9e0aaefb93c60a4be329d1d78
バントウフウマ <coolishpoo3@gmail.com>
2021-03-19 11:18:37 +0900

    modification users view

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -4,12 +4,13 @@
     <router-link to="/users/1" class="link">ユーザ１</router-link>
     <router-link to="/users/2" class="link">ユーザ2</router-link>
     <hr />
-    <h3>ユーザ{{ $route.params.id }}</h3>
+    <h3>ユーザ{{ id }}</h3>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["id"],
   watch: {
     $route() {
       console.log("ユーザが変わりました！");
