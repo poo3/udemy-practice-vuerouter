5888f1e16f97a876045b7d415274ccc72e0672c8
バントウフウマ <coolishpoo3@gmail.com>
2021-03-24 19:37:37 +0900

    setting vuex app

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1>Home</h1>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.state.count++;
+    },
+    decrement() {
+      this.$store.state.count--;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,24 @@
   <div>
     <h1>Home</h1>
     <button @click="toUsers">usersのページへ行く</button>
+    <p>{{ count }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
   methods: {
-    toUsers(){
+    toUsers() {
       this.$router.push({
         name: "users-id-profile",
-        params: {id: 1}
-      })
-    }
-  }
-}
+        params: { id: 1 },
+      });
+    },
+  },
+};
 </script>
