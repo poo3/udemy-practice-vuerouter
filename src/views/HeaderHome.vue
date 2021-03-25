af2f970b1eccdd3cfd6920d81d9d095db34000fb
バントウフウマ <coolishpoo3@gmail.com>
2021-03-25 17:06:04 +0900

    use mapMutations

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
     <h1>Home</h1>
-    <button @click="increment">次の倍数</button>
-    <button @click="decrement">前の倍数</button>
+    <button @click="increment(1)">次の倍数</button>
+    <button @click="decrement(1)">前の倍数</button>
   </div>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 export default {
   methods: {
-    increment() {
-      this.$store.commit("increment", 1);
-    },
-    decrement() {
-      this.$store.commit("decrement", 1);
-    },
+    ...mapMutations(["increment", "decrement"]),
   },
 };
 </script>
